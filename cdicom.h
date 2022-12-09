787124b3b0d1c3981b9cc612785af00cd8613e06
Darshan Pai <darshanp20@yahoo.com>
2022-12-08 21:48:38 -0600

    Updates to CDicom and ITKDicom

--- cdicom.h
+++ cdicom.h
@@ -21,15 +21,15 @@ protected:
 
 public:
     virtual ~CDicom() = default;
-    virtual void OpenDicomDir(std::string ) = 0;
+    virtual void OpenDicomDir(const std::string&) = 0;
     virtual InputImage4DType::Pointer GetImage4D() = 0;
     virtual bool isDicomLoaded() = 0;
-    virtual std::string& GetDicomDir() = 0;
-    virtual std::string GetDescription() = 0;
+    virtual const std::string& GetDicomDir() = 0;
+    virtual const std::string& GetDescription() = 0;
 	virtual unsigned int Get4thDimension() = 0;
 	virtual void Clean() = 0;
 	virtual InputImageType::Pointer GetImage3D(int) = 0;
-	virtual bool GetImageParams(int, std::map<std::string, std::string> &) {
+	virtual bool GetImageParams(int, std::map<std::string, std::string>&) {
 		return true;
 	}
 	virtual void SaveToDicom(const InputImageType::Pointer& Image, int volID, const QString& Dir, float, float, float)
