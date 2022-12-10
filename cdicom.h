bf380ab5e5cfa2aa24518a5bbc9fe44efd5578d6
Darshan Pai <darshanp20@yahoo.com>
2022-12-09 20:27:14 -0600

    Update indentation to spaces

--- cdicom.h
+++ cdicom.h
@@ -26,17 +26,17 @@ public:
     virtual bool isDicomLoaded() = 0;
     virtual const std::string& GetDicomDir() = 0;
     virtual const std::string& GetDescription() = 0;
-	virtual unsigned int Get4thDimension() = 0;
-	virtual void Clean() = 0;
-	virtual InputImageType::Pointer GetImage3D(int) = 0;
-	virtual bool GetImageParams(int, std::map<std::string, std::string>&) {
-		return true;
-	}
-	virtual void SaveToDicom(const InputImageType::Pointer& Image, int volID, const QString& Dir, float, float, float)
-	{
-	}
-	virtual void GetImageXYOrientation(double *dir) 
-	{
-		dir[0] = dir[1] = 1;
-	}
+    virtual unsigned int Get4thDimension() = 0;
+    virtual void Clean() = 0;
+    virtual InputImageType::Pointer GetImage3D(int) = 0;
+    virtual bool GetImageParams(int, std::map<std::string, std::string>&) {
+        return true;
+    }
+    virtual void SaveToDicom(const InputImageType::Pointer& Image, int volID, const QString& Dir, float, float, float)
+    {
+    }
+    virtual void GetImageXYOrientation(double *dir) 
+    {
+        dir[0] = dir[1] = 1;
+    }
 };
